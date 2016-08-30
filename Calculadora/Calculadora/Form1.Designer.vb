93aa7362845b9e5e43ab239c317260ce84fc3d3b
ThiagoZeva2 <santizeva2@gmail.com>
2016-08-29 19:18:16 -0500

    Add files via upload

---
+++ Calculadora/Calculadora/Form1.Designer.vb
@@ -0,0 +1,320 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Calculadora
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Calculadora))
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnpunto = New System.Windows.Forms.Button()
+        Me.btnmas = New System.Windows.Forms.Button()
+        Me.btnmenos = New System.Windows.Forms.Button()
+        Me.btnmulti = New System.Windows.Forms.Button()
+        Me.btndivi = New System.Windows.Forms.Button()
+        Me.btnigual = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnborrar = New System.Windows.Forms.Button()
+        Me.btnraiz = New System.Windows.Forms.Button()
+        Me.txtresu = New System.Windows.Forms.RichTextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn7.Location = New System.Drawing.Point(9, 60)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(45, 35)
+        Me.btn7.TabIndex = 0
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn8.Location = New System.Drawing.Point(60, 60)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(45, 35)
+        Me.btn8.TabIndex = 1
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn9.Location = New System.Drawing.Point(111, 60)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(45, 35)
+        Me.btn9.TabIndex = 2
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn4.Location = New System.Drawing.Point(9, 101)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(45, 35)
+        Me.btn4.TabIndex = 3
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn6.Location = New System.Drawing.Point(111, 101)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(45, 35)
+        Me.btn6.TabIndex = 5
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn1.Location = New System.Drawing.Point(9, 142)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(45, 35)
+        Me.btn1.TabIndex = 6
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn5.Location = New System.Drawing.Point(60, 101)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(45, 35)
+        Me.btn5.TabIndex = 7
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn2.Location = New System.Drawing.Point(60, 142)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(45, 35)
+        Me.btn2.TabIndex = 8
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn3.Location = New System.Drawing.Point(111, 142)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(45, 35)
+        Me.btn3.TabIndex = 9
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn0.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btn0.Location = New System.Drawing.Point(9, 183)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(96, 35)
+        Me.btn0.TabIndex = 10
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnpunto
+        '
+        Me.btnpunto.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnpunto.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btnpunto.Location = New System.Drawing.Point(111, 183)
+        Me.btnpunto.Name = "btnpunto"
+        Me.btnpunto.Size = New System.Drawing.Size(45, 35)
+        Me.btnpunto.TabIndex = 11
+        Me.btnpunto.Text = "."
+        Me.btnpunto.UseVisualStyleBackColor = True
+        '
+        'btnmas
+        '
+        Me.btnmas.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnmas.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btnmas.Location = New System.Drawing.Point(162, 60)
+        Me.btnmas.Name = "btnmas"
+        Me.btnmas.Size = New System.Drawing.Size(45, 35)
+        Me.btnmas.TabIndex = 12
+        Me.btnmas.Text = "+"
+        Me.btnmas.UseVisualStyleBackColor = True
+        '
+        'btnmenos
+        '
+        Me.btnmenos.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnmenos.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btnmenos.Location = New System.Drawing.Point(162, 101)
+        Me.btnmenos.Name = "btnmenos"
+        Me.btnmenos.Size = New System.Drawing.Size(45, 35)
+        Me.btnmenos.TabIndex = 13
+        Me.btnmenos.Text = "-"
+        Me.btnmenos.UseVisualStyleBackColor = True
+        '
+        'btnmulti
+        '
+        Me.btnmulti.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnmulti.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btnmulti.Location = New System.Drawing.Point(162, 142)
+        Me.btnmulti.Name = "btnmulti"
+        Me.btnmulti.Size = New System.Drawing.Size(45, 35)
+        Me.btnmulti.TabIndex = 14
+        Me.btnmulti.Text = "*"
+        Me.btnmulti.UseVisualStyleBackColor = True
+        '
+        'btndivi
+        '
+        Me.btndivi.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btndivi.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btndivi.Location = New System.Drawing.Point(162, 183)
+        Me.btndivi.Name = "btndivi"
+        Me.btndivi.Size = New System.Drawing.Size(45, 35)
+        Me.btndivi.TabIndex = 15
+        Me.btndivi.Text = "÷"
+        Me.btndivi.UseVisualStyleBackColor = True
+        '
+        'btnigual
+        '
+        Me.btnigual.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnigual.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btnigual.Location = New System.Drawing.Point(213, 101)
+        Me.btnigual.Name = "btnigual"
+        Me.btnigual.Size = New System.Drawing.Size(45, 117)
+        Me.btnigual.TabIndex = 16
+        Me.btnigual.Text = "="
+        Me.btnigual.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Panel1.Controls.Add(Me.btnborrar)
+        Me.Panel1.Controls.Add(Me.btnraiz)
+        Me.Panel1.Controls.Add(Me.btn0)
+        Me.Panel1.Controls.Add(Me.btnigual)
+        Me.Panel1.Controls.Add(Me.btn1)
+        Me.Panel1.Controls.Add(Me.btnmas)
+        Me.Panel1.Controls.Add(Me.btnmenos)
+        Me.Panel1.Controls.Add(Me.btnmulti)
+        Me.Panel1.Controls.Add(Me.btndivi)
+        Me.Panel1.Controls.Add(Me.btn2)
+        Me.Panel1.Controls.Add(Me.btn4)
+        Me.Panel1.Controls.Add(Me.btn5)
+        Me.Panel1.Controls.Add(Me.btn7)
+        Me.Panel1.Controls.Add(Me.btnpunto)
+        Me.Panel1.Controls.Add(Me.btn8)
+        Me.Panel1.Controls.Add(Me.btn3)
+        Me.Panel1.Controls.Add(Me.btn9)
+        Me.Panel1.Controls.Add(Me.btn6)
+        Me.Panel1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.Panel1.Location = New System.Drawing.Point(12, 70)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(266, 234)
+        Me.Panel1.TabIndex = 17
+        '
+        'btnborrar
+        '
+        Me.btnborrar.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnborrar.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.btnborrar.Location = New System.Drawing.Point(9, 19)
+        Me.btnborrar.Name = "btnborrar"
+        Me.btnborrar.Size = New System.Drawing.Size(249, 35)
+        Me.btnborrar.TabIndex = 20
+        Me.btnborrar.Text = "CE"
+        Me.btnborrar.UseVisualStyleBackColor = True
+        '
+        'btnraiz
+        '
+        Me.btnraiz.Font = New System.Drawing.Font("Lucida Fax", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnraiz.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btnraiz.Location = New System.Drawing.Point(213, 60)
+        Me.btnraiz.Name = "btnraiz"
+        Me.btnraiz.Size = New System.Drawing.Size(45, 35)
+        Me.btnraiz.TabIndex = 17
+        Me.btnraiz.Text = "√"
+        Me.btnraiz.UseVisualStyleBackColor = True
+        '
+        'txtresu
+        '
+        Me.txtresu.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.txtresu.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtresu.Location = New System.Drawing.Point(12, 12)
+        Me.txtresu.Name = "txtresu"
+        Me.txtresu.Size = New System.Drawing.Size(264, 42)
+        Me.txtresu.TabIndex = 19
+        Me.txtresu.Text = ""
+        '
+        'Calculadora
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(288, 360)
+        Me.Controls.Add(Me.txtresu)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Calculadora"
+        Me.Text = "Calculadora"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnpunto As Button
+    Friend WithEvents btnmas As Button
+    Friend WithEvents btnmenos As Button
+    Friend WithEvents btnmulti As Button
+    Friend WithEvents btndivi As Button
+    Friend WithEvents btnigual As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnraiz As Button
+    Friend WithEvents btnborrar As Button
+    Friend WithEvents txtresu As RichTextBox
+End Class
---
+++ Calculadora/Calculadora/Form1.vb
@@ -0,0 +1,160 @@
+﻿Public Class Calculadora
+
+    Dim resultado As Double
+    Dim op1 As Double
+    Dim op2 As Double
+    Dim ope As Double
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button3_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        txtresu.Text = txtresu.Text & “7”
+    End Sub
+
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles btn4.Click
+        txtresu.Text = txtresu.Text & “4”
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles btnigual.Click
+        op2 = Val(txtresu.Text)
+        Select Case ope
+            Case 1
+                resultado = op1 + op2
+            Case 2
+                resultado = op1 - op2
+            Case 3
+                resultado = op1 * op2
+            Case 4
+                If (op2 = 0) Then
+
+                    MsgBox("No se puede dividir entre 0")
+                Else
+                    resultado = op1 / op2
+                End If
+
+            Case 5
+                resultado = Math.Sqrt(op1)
+        End Select
+        txtresu.Text = resultado
+        ope = 0
+
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        txtresu.Text = txtresu.Text & “8”
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        txtresu.Text = txtresu.Text & “9”
+    End Sub
+
+    Private Sub btnmas_Click(sender As Object, e As EventArgs) Handles btnmas.Click
+
+
+        ope = 1
+        op1 = Val(txtresu.Text)
+
+        txtresu.Text = ""
+    End Sub
+
+    Private Sub btnraiz_Click(sender As Object, e As EventArgs) Handles btnraiz.Click
+        ope = 5
+
+        op1 = Val(txtresu.Text)
+        txtresu.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnmenos_Click(sender As Object, e As EventArgs) Handles btnmenos.Click
+        ope = 2
+        op1 = Val(txtresu.Text)
+        txtresu.Text = ""
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        txtresu.Text = txtresu.Text & “6”
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        txtresu.Text = txtresu.Text & “5”
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txtresu.Text = txtresu.Text & “1”
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        txtresu.Text = txtresu.Text & “2”
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        txtresu.Text = txtresu.Text & “3”
+    End Sub
+
+    Private Sub btnmulti_Click(sender As Object, e As EventArgs) Handles btnmulti.Click
+        ope = 3
+        op1 = Val(txtresu.Text)
+        txtresu.Text = ""
+    End Sub
+
+    Private Sub btndivi_Click(sender As Object, e As EventArgs) Handles btndivi.Click
+        ope = 4
+        op1 = Val(txtresu.Text)
+        txtresu.Text = ""
+    End Sub
+
+    Private Sub btnpunto_Click(sender As Object, e As EventArgs) Handles btnpunto.Click
+
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txtresu.Text = txtresu.Text & “0”
+    End Sub
+
+    Private Sub btnborrar_Click(sender As Object, e As EventArgs) Handles btnborrar.Click
+        txtresu.Text = ""
+
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+
+
+    Private Sub txtresu_TextChanged(sender As Object, e As EventArgs) Handles txtresu.TextChanged
+
+    End Sub
+
+    Private Sub txtresu_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtresu.KeyPress
+        If Not IsNumeric(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
